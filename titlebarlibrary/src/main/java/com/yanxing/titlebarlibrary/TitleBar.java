d4462cbeb4361cfe3e6df5e91ba9abc869185282
lishuangxiang <lyxtime@163.com>
2016-02-23 16:43:20 +0800

    添加更多属性

--- titlebarlibrary/src/main/java/com/yanxing/titlebarlibrary/TitleBar.java
+++ titlebarlibrary/src/main/java/com/yanxing/titlebarlibrary/TitleBar.java
@@ -3,12 +3,15 @@ package com.yanxing.titlebarlibrary;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -20,6 +23,7 @@ public class TitleBar extends LinearLayout {
 
     //返回
     private LinearLayout mBack;
+    private ImageView mBackImg;
     //标题
     private TextView mTitle;
     //右文字菜单
@@ -43,30 +47,47 @@ public class TitleBar extends LinearLayout {
         mTitle = (TextView) findViewById(R.id.title);
         mRightTitle = (TextView) findViewById(R.id.right_title);
         mBackground = (LinearLayout) findViewById(R.id.background);
+        mBackImg= (ImageView) findViewById(R.id.back_img);
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.MyTitleBar);
         //标题
         if (a.hasValue(0)){
             mTitle.setText(a.getText(0));
         }
-        //左菜单
-        if (a.hasValue(1)){
+        //标题居中true
+        if (a.getBoolean(1,false)){
+            setTitleCenter();
+        }
+        //标题颜色
+        if (a.hasValue(2)){
+            mTitle.setTextColor(a.getColor(R.styleable.MyTitleBar_title_main_color,0));
+        }
+        //右菜单
+        if (a.hasValue(3)){
             mRightTitle.setText(a.getText(1));
         }
-        //做菜单可见性
-        if (a.hasValue(2)){
+        //右菜单可见true,默认不可见false
+        if (a.getBoolean(4,false)){
             mRightTitle.setVisibility(VISIBLE);
         }
         //背景颜色
-        if (a.hasValue(R.styleable.MyTitleBar_backgroundColor)){
+        if (a.hasValue(5)){
             Drawable background = a.getDrawable(R.styleable.MyTitleBar_backgroundColor);
             mBackground.setBackgroundDrawable(background);
         }
-        mBack.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ((Activity) context).finish();
-            }
-        });
+        //返回图标不可见false,默认可见true
+        if (!a.getBoolean(6,true)){
+            mBackImg.setVisibility(INVISIBLE);
+        }
+
+        //返回图标可见，才有点击事件
+        if (a.getBoolean(6,true)){
+            mBack.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ((Activity) context).finish();
+                }
+            });
+        }
     }
 
     /**
@@ -86,6 +107,13 @@ public class TitleBar extends LinearLayout {
     }
 
     /**
+     * 设置标题文字颜色
+     */
+    private void setTitleColor(int  color) {
+        mTitle.setTextColor(color);
+    }
+
+    /**
      * 设置标题文字大小
      */
     private void setTitleSize(float size) {
@@ -96,9 +124,9 @@ public class TitleBar extends LinearLayout {
      * 设置标题居中，默认左边
      */
     private void setTitleCenter() {
-        LayoutParams params = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        params.setMargins(0, 0, 0, 0);
-        mTitle.setLayoutParams(params);
+        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        lp.setMargins(0, 0, 0, 0);
+        mTitle.setLayoutParams(lp);
         mTitle.setGravity(Gravity.CENTER);
     }
 
