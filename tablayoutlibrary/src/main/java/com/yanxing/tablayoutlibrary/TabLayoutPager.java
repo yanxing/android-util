8fcc76568f954e6924dc464359d0632acd58dd4e
lishuangxiang <lyxtime@163.com>
2016-03-14 17:58:26 +0800

    修复bug

--- tablayoutlibrary/src/main/java/com/yanxing/tablayoutlibrary/TabLayoutPager.java
+++ tablayoutlibrary/src/main/java/com/yanxing/tablayoutlibrary/TabLayoutPager.java
@@ -10,6 +10,7 @@ import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.FrameLayout;
+import com.yanxing.tablayoutlibrary.R;
 
 import java.util.List;
 
