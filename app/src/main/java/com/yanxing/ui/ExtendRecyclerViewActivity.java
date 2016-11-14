cec20df377553cd6f5c64793dfadb0ede80a54b4
lishuangxiang <lyxtime@163.com>
2016-11-14 11:44:11 +0800

    扩展RecyclerView,增加禁止上下滑动和滑动到指定位置

--- app/src/main/java/com/yanxing/ui/ExtendRecyclerViewActivity.java
+++ app/src/main/java/com/yanxing/ui/ExtendRecyclerViewActivity.java
@@ -1,32 +1,33 @@
 package com.yanxing.ui;
 
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
 import com.yanxing.adapterlibrary.RecyclerViewAdapter;
 import com.yanxing.base.BaseActivity;
-import com.yanxing.view.ExtendRecyclerView;
+import com.yanxing.view.CustomLinearLayoutManager;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
- * 扩展RecyclerView,增加滚动到指定position和禁止上下拉滑动
+ * 扩展RecyclerView,增加禁止上下滑动和滑动到指定位置(参考http://blog.csdn.net/tyzlmjj/article/details/49227601)
  * Created by lishuangxiang on 2016/11/11.
  */
-
 public class ExtendRecyclerViewActivity extends BaseActivity {
 
     @BindView(R.id.recyclerView)
     RecyclerView mRecyclerView;
 
-    RecyclerViewAdapter mRecyclerViewAdapter;
-    private List<String> mStrings=new ArrayList<>();
+    private RecyclerViewAdapter mRecyclerViewAdapter;
+    private CustomLinearLayoutManager mLayoutManager;
+    private boolean mNeedScroll;
+    private List<String> mStrings = new ArrayList<>();
+    private int mPosition = 20;
 
     @Override
     protected int getLayoutResID() {
@@ -35,43 +36,85 @@ public class ExtendRecyclerViewActivity extends BaseActivity {
 
     @Override
     protected void afterInstanceView() {
-        for (int i=0;i<50;i++){
+        for (int i = 0; i < 50; i++) {
             mStrings.add(String.valueOf(i));
         }
-        //线性
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mRecyclerViewAdapter = new RecyclerViewAdapter<String>(mStrings,R.layout.adapter_recycler_view){
+        mLayoutManager = new CustomLinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerViewAdapter = new RecyclerViewAdapter<String>(mStrings, R.layout.adapter_recycler_view) {
 
             @Override
             public void onBindViewHolder(RecyclerViewAdapter.MyViewHolder holder, final int position) {
-                TextView textView= (TextView) holder.findViewById(R.id.text);
+                TextView textView = (TextView) holder.findViewById(R.id.text);
                 textView.setText(mStrings.get(position));
                 holder.itemView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        showToast(getString(R.string.you_click)+(position+1)+getString(R.string.ge));
+                        showToast(getString(R.string.you_click) + (position + 1) + getString(R.string.ge));
                     }
                 });
                 holder.itemView.setOnLongClickListener(v -> {
-                    showToast(getString(R.string.long_an)+(position+1)+getString(R.string.ge));
+                    showToast(getString(R.string.long_an) + (position + 1) + getString(R.string.ge));
                     return true;
                 });
             }
         };
         mRecyclerView.setAdapter(mRecyclerViewAdapter);
-        setScrollEnabled(true);
+        mRecyclerView.addOnScrollListener(new RecyclerViewListener());
     }
 
-    public void setScrollEnabled(boolean enabled){
-        try {
-            Class c = Class.forName("android.support.v7.widget.RecyclerView");
-            Field fs = c.getDeclaredField("mLayout");
-            fs.setAccessible(true);
-//            fs.get
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
+    @OnClick({R.id.canScroll, R.id.noScroll, R.id.scrollPosition})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.canScroll:
+                mLayoutManager.setScrollEnable(true);
+                mRecyclerView.setLayoutManager(mLayoutManager);
+                break;
+            case R.id.noScroll:
+                mLayoutManager.setScrollEnable(false);
+                mRecyclerView.setLayoutManager(mLayoutManager);
+                break;
+            case R.id.scrollPosition:
+                mPosition = 20;
+                moveToPosition(mPosition);
+                break;
         }
+    }
+
+    /**
+     * 滑动监听，当指定位置位于最后可见项的后面时，移动最后的距离
+     */
+    class RecyclerViewListener extends RecyclerView.OnScrollListener {
+        @Override
+        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+            super.onScrolled(recyclerView, dx, dy);
+            if (mNeedScroll) {
+                mNeedScroll = false;
+                int top = mRecyclerView.getChildAt(mPosition - mLayoutManager.findFirstVisibleItemPosition()).getTop();
+                mRecyclerView.scrollBy(0, top);
+            }
+        }
+    }
+
+    /**
+     * 指定位置置顶
+     *
+     * @param n
+     */
+    private void moveToPosition(int n) {
+        int firstItem = mLayoutManager.findFirstVisibleItemPosition();
+        int lastItem = mLayoutManager.findLastVisibleItemPosition();
+        //当要置顶的项在当前显示的第一个项的前面时
+        if (n <= firstItem) {
+            mRecyclerView.scrollToPosition(n);
+        } else if (n <= lastItem) {//当要置顶的项已经在屏幕上显示时
+            int top = mRecyclerView.getChildAt(n - firstItem).getTop();
+            mRecyclerView.scrollBy(0, top);
+        } else {
+            //当要置顶的项在当前显示的最后一项的后面时,先显示出来
+            mRecyclerView.scrollToPosition(n);
+            mNeedScroll = true;
+        }
+
     }
 }
---
+++ app/src/main/java/com/yanxing/view/CustomLinearLayoutManager.java
@@ -0,0 +1,30 @@
+package com.yanxing.view;
+
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+
+/**
+ * 自定义线性布局管理器，添加禁止上下滑动
+ * Created by lishuangxiang on 2016/11/14.
+ */
+public class CustomLinearLayoutManager extends LinearLayoutManager {
+
+    private boolean mCanScrollEnable=true;
+
+    public CustomLinearLayoutManager(Context context) {
+        super(context);
+    }
+
+    /**
+     * 禁止上下滑动
+     * @param enable false禁止滑动
+     */
+    public void setScrollEnable(boolean enable){
+        this.mCanScrollEnable=enable;
+    }
+
+    @Override
+    public boolean canScrollVertically() {
+        return super.canScrollVertically()&&mCanScrollEnable;
+    }
+}
