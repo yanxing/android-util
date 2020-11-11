cbfb22c6ca612077854d6d39983ced1351488d20
李双祥 <lyxtime@163.com>
2020-11-11 17:58:02 +0800

    Update FileUriUtil.java

--- app/src/main/java/com/yanxing/util/FileUriUtil.java
+++ app/src/main/java/com/yanxing/util/FileUriUtil.java
@@ -16,23 +16,23 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 /**
- * target androidQ时，本地图片uri和file互相转化
+ * target androidQ时，本地uri和file互相转化
+ *
  * @author 李双祥 on 2019/10/31.
  */
 public class FileUriUtil {
 
     /**
-     * uri转file
+     * 图片/视频file形式转Uri，兼容AndroidQ
      *
-     * @param context
-     * @param uri
-     * @return
+     * @param type 1图片，2视频
      */
-    public static String getFilePath(final Context context, final Uri uri) {
-        if (Build.VERSION.SDK_INT >= 29) {//androidQ
-            return getAndroidQRealFilePath(context, uri);
+    public static String getUriToFile(final Context context, final Uri uri, int type) {
+        //androidQ
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            return getNewUriToFile(context, uri, type);
         }
-        return getRealFilePath(context, uri);
+        return getOldUriToFile(context, uri, type);
 
     }
 
@@ -43,132 +43,183 @@ public class FileUriUtil {
      * @param uri
      * @return 文件路径
      */
-    private static String getRealFilePath(final Context context, final Uri uri) {
-        if (null == uri) return null;
+    private static String getOldUriToFile(final Context context, final Uri uri, int type) {
+        if (null == uri) {
+            return null;
+        }
         final String scheme = uri.getScheme();
         String data = null;
-        if (scheme == null)
+        if (scheme == null) {
             data = uri.getPath();
-        else if (ContentResolver.SCHEME_FILE.equals(scheme)) {
+        } else if (ContentResolver.SCHEME_FILE.equals(scheme)) {
             data = uri.getPath();
         } else if (ContentResolver.SCHEME_CONTENT.equals(scheme)) {
-            Cursor cursor = context.getContentResolver()
-                    .query(uri, new String[]{MediaStore.Images.ImageColumns.DATA}, null, null, null);
-            if (null != cursor) {
-                if (cursor.moveToFirst()) {
+            if (type == 1) {
+                Cursor cursor = context.getContentResolver()
+                        .query(uri, new String[]{MediaStore.Images.ImageColumns.DATA}, null, null, null);
+                if (null != cursor && cursor.moveToFirst()) {
                     int index = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
                     if (index > -1) {
                         data = cursor.getString(index);
                     }
+                    cursor.close();
                 }
-                cursor.close();
+
+            } else {
+                Cursor cursor = context.getContentResolver()
+                        .query(uri, new String[]{MediaStore.Video.VideoColumns.DATA}, null, null, null);
+                if (cursor != null && cursor.moveToFirst()) {
+                    int index = cursor.getColumnIndex(MediaStore.Video.VideoColumns.DATA);
+                    if (index > -1) {
+                        data = cursor.getString(index);
+                    }
+                    cursor.close();
+                }
             }
         }
         return data;
     }
 
     /**
-     * AndroidQ URI转化图片路径
+     * androidQ file转Uri
      *
-     * @param context
-     * @param uri
-     * @return
+     * @param type 1图片，2视频
      */
     @RequiresApi(api = 29)
-    private static String getAndroidQRealFilePath(final Context context, final Uri uri) {
-        if (null == uri) return null;
+    private static String getNewUriToFile(final Context context, final Uri uri, int type) {
+        if (null == uri) {
+            return null;
+        }
         final String scheme = uri.getScheme();
         String data = null;
-        if (scheme == null)
+        if (scheme == null) {
             data = uri.getPath();
-        else if (ContentResolver.SCHEME_FILE.equals(scheme)) {
+        } else if (ContentResolver.SCHEME_FILE.equals(scheme)) {
             data = uri.getPath();
         } else if (ContentResolver.SCHEME_CONTENT.equals(scheme)) {
-            Cursor cursor = context.getContentResolver()
-                    .query(uri, new String[]{MediaStore.Images.ImageColumns.RELATIVE_PATH, MediaStore.Images.Media.DISPLAY_NAME}, null, null, null);
-            if (null != cursor) {
-                if (cursor.moveToFirst()) {
+            if (type == 1) {
+                Cursor cursor = context.getContentResolver()
+                        .query(uri, new String[]{MediaStore.Images.ImageColumns.RELATIVE_PATH, MediaStore.Images.Media.DISPLAY_NAME}, null, null, null);
+                if (cursor != null && cursor.moveToFirst()) {
                     String name = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME));
                     String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.RELATIVE_PATH));
+                    cursor.close();
                     return path + name;
                 }
-                cursor.close();
+            } else {
+                Cursor cursor = context.getContentResolver()
+                        .query(uri, new String[]{MediaStore.Video.VideoColumns.RELATIVE_PATH, MediaStore.Video.Media.DISPLAY_NAME}
+                                , null, null, null);
+                if (cursor != null && cursor.moveToFirst()) {
+                    String name = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DISPLAY_NAME));
+                    String path = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.RELATIVE_PATH));
+                    cursor.close();
+                    return path + name;
+                }
             }
         }
         return data;
     }
 
     /**
-     * file转Uri，针对android Q存储权限改动，使用uri访问图片
+     * 图片/视频file形式转Uri，兼容AndroidQ
      *
-     * @param context
-     * @param imageFile
-     * @return
+     * @param type 1图片，2视频
      */
-    public static Uri getImageContentUri(Context context, File imageFile) {
-        if (Build.VERSION.SDK_INT >= 29) {//androidQ
-            return getAndroidQContentUri(context, imageFile);
+    public static Uri getFileToUri(Context context, File file, int type) {
+        //androidQ
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            return getNewFileToUri(context, file, type);
         }
-        return getContentUri(context, imageFile);
+        return getOldFileToUri(context, file, type);
     }
 
     /**
      * androidQ以前 file转Uri
      *
-     * @param context
-     * @param imageFile
-     * @return
+     * @param type 1图片，2视频
      */
-    private static Uri getContentUri(Context context, File imageFile) {
-        String filePath = imageFile.getAbsolutePath();
-        Cursor cursor = context.getContentResolver().query(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                new String[]{MediaStore.Images.Media._ID},
-                MediaStore.Images.Media.DATA + "=? ",
-                new String[]{filePath}, null);
-        if (cursor != null && cursor.moveToFirst()) {
-            int id = cursor.getInt(cursor
-                    .getColumnIndex(MediaStore.MediaColumns._ID));
-            Uri baseUri = Uri.parse("content://media/external/images/media");
-            return Uri.withAppendedPath(baseUri, "" + id);
+    private static Uri getOldFileToUri(Context context, File file, int type) {
+        String filePath = file.getAbsolutePath();
+        if (type == 1) {
+            Cursor cursor = context.getContentResolver().query(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    new String[]{MediaStore.Images.Media._ID},
+                    MediaStore.Images.Media.DATA + "=? ",
+                    new String[]{filePath}, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                int id = cursor.getInt(cursor
+                        .getColumnIndex(MediaStore.MediaColumns._ID));
+                Uri baseUri = Uri.parse("content://media/external/images/media");
+                return Uri.withAppendedPath(baseUri, "" + id);
+            } else {
+                //不再对MediaStore.Images.Media.EXTERNAL_CONTENT_URI进行插入数据，再返回content://,若图片被删除，将会冗余
+                // 另外Android Q上私有目录图片不能插入到MediaStore.Images.Media.EXTERNAL_CONTENT_URI，以及沙盒外图片都需要授权，
+                // 所以如果不存在此处不再转为content://
+                return Uri.parse(filePath);
+            }
         } else {
-            //不再对MediaStore.Images.Media.EXTERNAL_CONTENT_URI进行插入数据，再返回content://,若图片被删除，将会冗余
-            // 另外Android Q上私有目录图片不能插入到MediaStore.Images.Media.EXTERNAL_CONTENT_URI，以及沙盒外图片都需要授权，
-            // 所以如果不存在此处不再转为content://
-            return Uri.parse(filePath);
+            Cursor cursor = context.getContentResolver().query(
+                    MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                    new String[]{MediaStore.Video.Media._ID},
+                    MediaStore.Video.Media.DATA + "=? ",
+                    new String[]{filePath}, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                int id = cursor.getInt(cursor
+                        .getColumnIndex(MediaStore.MediaColumns._ID));
+                Uri baseUri = Uri.parse("content://media/external/video/media");
+                return Uri.withAppendedPath(baseUri, "" + id);
+            } else {
+                return Uri.parse(filePath);
+            }
         }
     }
 
     /**
      * androidQ file转Uri
      *
-     * @param context
-     * @param imageFile
-     * @return
+     * @param type 1图片，2视频
      */
-
     @RequiresApi(api = 29)
-    private static Uri getAndroidQContentUri(Context context, File imageFile) {
-        if (imageFile == null) {
+    private static Uri getNewFileToUri(Context context, File file, int type) {
+        if (file == null) {
             return null;
         }
-        String filePath = imageFile.getAbsolutePath();
+        String filePath = file.getAbsolutePath();
         String path = filePath.substring(0, filePath.lastIndexOf("/") + 1);
-        Cursor cursor = context.getContentResolver().query(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                new String[]{MediaStore.Images.Media._ID},
-                MediaStore.Images.Media.DISPLAY_NAME + "=? and " + MediaStore.Images.Media.RELATIVE_PATH + "=? ",
-                new String[]{imageFile.getName(), path}, null);
-        if (cursor != null && cursor.moveToFirst()) {
-            int id = cursor.getInt(cursor
-                    .getColumnIndex(MediaStore.MediaColumns._ID));
-            Uri baseUri = Uri.parse("content://media/external/images/media");
-            return Uri.withAppendedPath(baseUri, "" + id);
+        if (type == 1) {
+            Cursor cursor = context.getContentResolver().query(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    new String[]{MediaStore.Images.Media._ID},
+                    MediaStore.Images.Media.DISPLAY_NAME + "=? and " + MediaStore.Images.Media.RELATIVE_PATH + "=? ",
+                    new String[]{file.getName(), path}, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                int id = cursor.getInt(cursor
+                        .getColumnIndex(MediaStore.MediaColumns._ID));
+                Uri baseUri = Uri.parse("content://media/external/images/media");
+                cursor.close();
+                return Uri.withAppendedPath(baseUri, "" + id);
+            } else {
+                //不再对MediaStore.Images.Media.EXTERNAL_CONTENT_URI进行插入数据，再返回content://,若图片被删除，将会冗余，
+                // 另外Android Q上私有目录图片不能插入到MediaStore.Images.Media.EXTERNAL_CONTENT_URI，以及沙盒外图片都需要授权，
+                // 所以如果不存在此处不再转为content://
+                return Uri.parse(filePath);
+            }
         } else {
-            //不再对MediaStore.Images.Media.EXTERNAL_CONTENT_URI进行插入数据，再返回content://,若图片被删除，将会冗余，
-            // 另外Android Q上私有目录图片不能插入到MediaStore.Images.Media.EXTERNAL_CONTENT_URI，以及沙盒外图片都需要授权，
-            // 所以如果不存在此处不再转为content://
-            return Uri.parse(filePath);
+            Cursor cursor = context.getContentResolver().query(
+                    MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                    new String[]{MediaStore.Video.Media._ID},
+                    MediaStore.Video.Media.DISPLAY_NAME + "=? and " + MediaStore.Video.Media.RELATIVE_PATH + "=? ",
+                    new String[]{file.getName(), path}, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                int id = cursor.getInt(cursor
+                        .getColumnIndex(MediaStore.MediaColumns._ID));
+                Uri baseUri = Uri.parse("content://media/external/video/media");
+                cursor.close();
+                return Uri.withAppendedPath(baseUri, "" + id);
+            } else {
+                return Uri.parse(filePath);
+            }
         }
     }
 
