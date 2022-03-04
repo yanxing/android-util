65a2724d1f67927b7ccb7dd9e6a486e82fd9b767
lishuangxiang <lyxtime@163.com>
2022-03-04 17:24:10 +0800

    delete

--- app/src/main/java/com/yanxing/util/FileUtil.java
+++ app/src/main/java/com/yanxing/util/FileUtil.java
@@ -6,71 +6,54 @@ import android.os.Environment;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
- * 磁盘数据存储操作
+ * 磁盘数据存储操作,仅限于App沙盒内文件操作
  * 由于androidQ存储权限更改，使用SAF操作
+ *
  * @see com.yanxing.util.DocumentFileUtil
  * Created by lishuangxiang on 2016/1/25.
  */
 public class FileUtil {
 
-    /**
-     * 获取android文件系统，外置存储
-     */
-    private static final String STORAGE_PATH = Environment.getExternalStorageDirectory() + "/";
-    private static final String TAG = "FileUtil";
-
-    /**
-     * 获取存储根目录
-     *
-     * @return
-     */
-    public static String getStoragePath() {
-        return STORAGE_PATH;
-    }
+    @Deprecated
+    public static final String STORAGE_PATH = Environment.getExternalStorageDirectory() + "/";
 
     /**
      * androidQ沙盒内缓存路径
-     * @param context
-     * @return
      */
-    public static String getCachePath(Context context){
-        File file=context.getExternalCacheDir();
+    public static String getCachePath(Context context) {
+        File file = context.getExternalCacheDir();
         return file.toString();
     }
 
     /**
      * androidQ沙盒内存放文件路径
-     * @param context
-     * @return
      */
-    public static String getFilesPath(Context context){
-        File file=context.getExternalFilesDir("");
+    public static String getFilesPath(Context context) {
+        File file = context.getExternalFilesDir("");
         return file.toString();
     }
 
     /**
-     * 在创建文件，绝对路径
-     *
-     * @throws IOException
+     * 沙盒内创建文件，绝对路径
      */
     public static File createFile(String fileName) {
         File file = new File(fileName);
         try {
-            file.createNewFile();
+            boolean result = file.createNewFile();
+            if (result) return file;
         } catch (IOException e) {
         }
-        return file;
+        return null;
     }
 
     /**
-     * 文件是否存在，true存在,绝对路径
+     * 沙盒内文件是否存在，true存在,绝对路径
      *
      * @param filePath 文件路径
      * @return 存在 返回true 否则false
@@ -81,62 +64,54 @@ public class FileUtil {
     }
 
     /**
-     * 创建目录,如果存在，则不创建 ,绝对路径
+     * 沙盒创建目录,如果存在，则不创建 ,绝对路径
      *
      * @param dirName 文件路径
      */
-    public static File createDir(String dirName) {
+    public static boolean createDir(String dirName) {
         File dir = new File(dirName);
-        dir.mkdir();
-        return dir;
+        return dir.mkdir();
     }
 
     /**
-     * 根据路径删除图片,绝对路径
-     *
-     * @param path
+     * 沙盒内删除文件,绝对路径
      */
-    public static void deleteFile(String path) {
+    public static boolean deleteFile(String path) {
         File file = new File(path);
-        file.delete();
+        return file.delete();
     }
 
     /**
-     * 将一个InputStream里面的数据写入到外置存储中,绝对路径
+     * 将一个InputStream里面的数据写入到沙盒存储中,绝对路径
      *
      * @param path     路径名
      * @param fileName 文件名
      */
-    public static File writeStInput(String path, String fileName, InputStream input) {
-        File file = null;
-        OutputStream output = null;
+    public static boolean writeStInput(String path, String fileName, InputStream input) {
+        File file;
+        OutputStream output;
         try {
             createDir(path);
             file = createFile(path + fileName);
-            output = new FileOutputStream(file);
-            byte buffer[] = new byte[4 * 1024];
-            int offset;
-            while ((offset = input.read(buffer)) != -1) {
-                output.write(buffer, 0, offset);
-            }
-            output.flush();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
+            if (file != null) {
+                output = new FileOutputStream(file);
+                byte[] buffer = new byte[4 * 1024];
+                int offset;
+                while ((offset = input.read(buffer)) != -1) {
+                    output.write(buffer, 0, offset);
+                }
+                output.flush();
                 output.close();
-            } catch (Exception e) {
-                e.printStackTrace();
+                return true;
             }
+            return false;
+        } catch (Exception e) {
+            return false;
         }
-        return file;
     }
 
     /**
-     * file文件转成字节流
-     *
-     * @param filePath
-     * @return
+     * 沙盒file文件转成字节流
      */
     public static byte[] getBytes(String filePath) {
         byte[] buffer = null;
@@ -152,25 +127,9 @@ public class FileUtil {
             fis.close();
             bos.close();
             buffer = bos.toByteArray();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
-            e.printStackTrace();
         }
         return buffer;
     }
 
-    /**
-     * 重命名文件
-     *
-     * @param path    文件本地路径 比如Environment.getExternalStorageDirectory() + "/123.png"
-     * @param newName 文件新命名  456.png  结果Environment.getExternalStorageDirectory() + "/456.png"
-     */
-    public static void rename(String path, String newName) {
-        File file = new File(path);
-        if (file.exists()) {
-            File file1 = new File(path.substring(0, path.lastIndexOf("/") + 1) + newName);
-            file.renameTo(file1);
-        }
-    }
 }
